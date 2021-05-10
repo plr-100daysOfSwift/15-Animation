add35b9e82f849401e4857dc42c594174ac5fe6d
Paul Richardson <developer@photographerafoot.com>
2021-05-11 08:59:40 +1000

    CHG: Set up image view

--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	var imageView: UIImageView!
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+
+		imageView = UIImageView(image: UIImage(named: "penguin"))
+		imageView.center = CGPoint(x: 512, y: 384)
+		view.addSubview(imageView)
+
 	}
 
 	@IBAction func tapped(_ sender: Any) {
